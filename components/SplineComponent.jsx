707f9d63640eb906803bfad249b5dbfc927aa6d2
Ankit Gupta <gptankit159@gmail.com>
2022-08-28 16:39:31 +0530

    mobile view video test v3

--- components/SplineComponent.jsx
+++ components/SplineComponent.jsx
@@ -4,10 +4,6 @@ const SplineComponent =()=> {
 
     return (
     <div id='canvasContainer'>
-      {/* <video autoPlay loop width={400} style={{outline:"none", margin:"auto"}}>
-        <source src="./assets/robo.mp4"  type="video/mp4" />
-      </video> */}
-      {/* <img src="./assets/robo.gif" width={350}  /> */}
     <canvas id="canvas3d">
     </canvas>
   </div>
