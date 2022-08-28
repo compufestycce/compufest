f2c18e3bc15cf7fd0782c5c02960524d9d4475cd
Ankit Gupta <gptankit159@gmail.com>
2022-08-28 13:55:12 +0530

    mobile view video test v2

--- components/SplineComponent.jsx
+++ components/SplineComponent.jsx
@@ -7,6 +7,7 @@ const SplineComponent =()=> {
       {/* <video autoPlay loop width={400} style={{outline:"none", margin:"auto"}}>
         <source src="./assets/robo.mp4"  type="video/mp4" />
       </video> */}
+      {/* <img src="./assets/robo.gif" width={350}  /> */}
     <canvas id="canvas3d">
     </canvas>
   </div>
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,47 +1,36 @@
-
-    var myInterval = setInterval(afterload, 1000);
-    function afterload() {
-        try{
-            let  elem = document.getElementById("canvasContainer");  
-            if (!Modernizr.touch){                                                                                                                                                                                                                                                                                                                                                                                            elem = document.getElementById('canvas3d');
-            // var primeWorker = new Worker('/scripts/gltfLoader.js');
-            var app = new SpeRuntime.Application();
-            app.start("scripts/scene.gltf");
-            // window.onload=()=>{
-            // console.log("still running");
-            elem.style.width = "75%";
-            elem.style.height = "75%";
-            }else{
-                    // const canvas = document.getElementById("canvas3d");
-                    // const ctx = canvas.getContext("2d");
-                    // var myGif = GIF();
-                    // myGif.load("./assets/robo.gif");
-                    // ctx.drawImage(myGif.image,0,0);
-                    document.getElementById("canvas3d").remove();
-                    var c = document.getElementById ("canvasContainer");
-                    // Create an element <video>
-                    var v = document.createElement ("video");
-                    // Set the attributes of the video
-                    v.src = "./assets/robo.mp4";
-                    v.autoplay = true;
-                    v.loop = true;
-                    v.type="video/mp4";
-                    
-                    // Add the video to <div>
-                    c.appendChild(v);
-            }
-            stopAfterload();
-            // }
-        }catch(err) {
+let  elem = document.getElementById("canvasContainer");  
+console.log("!Modernizr.touch");
+if (window.innerWidth>=768){                                                                                                                                                                                                                                                                                                                                                                                            elem = document.getElementById('canvas3d');
+    // var primeWorker = new Worker('/scripts/gltfLoader.js');
+    var app = new SpeRuntime.Application();
+    app.start("scripts/scene.gltf");
+    // window.onload=()=>{
+    // console.log("still running");
+    elem.style.width = "75%";
+    elem.style.height = "75%";
+    console.log("windows");
+    }else{
+        var myInterval = setInterval(afterload, 1000);
+        function afterload() {
+            try{
+                document.getElementById("canvas3d").remove();
+                var c = document.getElementById ("canvasContainer");
+                // Create an element <video>
+                var v = document.createElement ("video");
+                // Set the attributes of the video
+                v.src = "./assets/robo.mp4";
+                v.autoplay = true;
+                v.loop = true;
+                v.type="video/mp4";
+                
+                // Add the video to <div>
+                c.appendChild(v);
+                stopAfterload();
+            }catch(err) {
             
+            }
         }
     }
-
-    function stopAfterload() {
-        clearInterval(myInterval);
-    }
-
-
-
-
-
+function stopAfterload() {
+    clearInterval(myInterval);
+}
